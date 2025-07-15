82640084c444c5b386bac7bf6dd07ecfb602f666
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-15 16:28:53 +0530

    handled edit task,if we click on edit button

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 function TodoList() {
     const todos=useSelector((state)=>state.todos)
     console.log(todos)
+    const [editId,setEditId]=useState(null);
+    const [editText,setEditText]=useState('');
+
+    const handleEditClick=(id,currentText)=>{
+        setEditId(id);
+        setEditText(currentText)
+    }
+
   return (
     <div className="container">
+        {editId}{editText}
       {
         todos.map((todo)=>(
             <div className="task-item" key={todo.id}>
-                <span>{todo.text}</span>
-                <button>Edit</button>
+                {
+                    editId===todo.id ?
+                    (
+                        <>
+                            <input
+                                type='text'
+                                value={editText}
+                                onChange={(e)=>setEditText(e.target.value)}
+                            />
+                            <button>Save</button>
+                        </>
+                    ) :
+                    (
+                        <>
+                            <span>{todo.text}</span>
+                            <button onClick={()=>handleEditClick(todo.id,todo.text)}>Edit</button>
+                        </>
+                    )
+                }
                 <button>Delete</button>
             </div>
         ))
